94b5dbdff318fe9ff1eb0b0b2623ead99c2f4e37
Nikita Ganzikov <nganzikov@gmail.com>
2025-08-24 00:11:20 +0300

    feat: enhance image resizing functionality to support aspect ratio maintenance and original image return

--- routes/image_resize_image.go
+++ routes/image_resize_image.go
@@ -7,7 +7,24 @@ import (
 )
 
 func resizeImage(img image.Image, width int, height int, interpolation resize.InterpolationFunction) (image.Image, error) {
-	resized := resize.Resize(uint(width), uint(height), img, interpolation)
+	// If both width and height are specified, resize to exact dimensions
+	if width > 0 && height > 0 {
+		resized := resize.Resize(uint(width), uint(height), img, interpolation)
+		return resized, nil
+	}
 
-	return resized, nil
+	// If only width is specified, maintain aspect ratio
+	if width > 0 && height == 0 {
+		resized := resize.Resize(uint(width), 0, img, interpolation)
+		return resized, nil
+	}
+
+	// If only height is specified, maintain aspect ratio
+	if width == 0 && height > 0 {
+		resized := resize.Resize(0, uint(height), img, interpolation)
+		return resized, nil
+	}
+
+	// If neither width nor height is specified, return original image
+	return img, nil
 }
